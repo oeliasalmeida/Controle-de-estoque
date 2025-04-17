103b654e1504d35eb91bbb6583d4ab4190144d86
Elias Almeida <51964431+oeliasalmeida@users.noreply.github.com>
2025-04-17 20:41:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Isso é CRUCIAL para caminhos relativos
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
